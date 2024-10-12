0180327695690b10d9ef544a1465b34f5b6b72bf
HoangTLL <tllhoang1512@gmail.com>
2024-10-12 12:22:20 +0700

    ADD API CARTRIP

---
+++ SharingTaxiProject/STP.Repository/Repository/CarTripRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMS.Repository.Base;
+using STP.Repository.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace STP.Repository.Repository
+{
+    public class CarTripRepository : GenericRepository<CarTrip>
+    {
+        public CarTripRepository(ShareTaxiContext context) : base(context) { }
+
+        public async Task<List<CarTrip>> GetAllCarTripsWithTripAsync()
+        {
+            return await _context.CarTrips
+                .Include(ct => ct.Trip)
+                .ToListAsync();
+        }
+
+        public async Task<CarTrip> GetCarTripWithTripByIdAsync(int id)
+        {
+            return await _context.CarTrips
+                .Include(ct => ct.Trip)
+                .FirstOrDefaultAsync(ct => ct.Id == id);
+        }
+
+        public async Task<CarTrip> GetByTripIdAsync(int tripId)
+        {
+            return await _context.CarTrips
+                .Include(ct => ct.Trip)
+                .FirstOrDefaultAsync(ct => ct.TripId == tripId);
+        }
+
+        public async Task AddAsync(CarTrip carTrip)
+        {
+            await _context.CarTrips.AddAsync(carTrip);
+        }
+    }
+}
